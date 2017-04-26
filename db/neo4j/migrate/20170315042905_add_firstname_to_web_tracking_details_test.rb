ea9cebd2e939ad33cdc6444aeafdab39cd65cb7b
snehalh <snehal.h@bewakoof.com>
2017-04-26 12:07:48 +0530

    delete unused file

--- db/neo4j/migrate/20170315042905_add_firstname_to_web_tracking_details_test.rb
+++
@@ -1,9 +0,0 @@
-class AddFirstnameToWebTrackingDetailsTest < Neo4j::Migrations::Base
-  def up
-    add_column :WebTrackingDetailsTest, :firstname, :String
-  end
-
-  def down
-    raise Neo4j::IrreversibleMigration
-  end
-end
